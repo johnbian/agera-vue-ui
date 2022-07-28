341b287dd8d2d7e87581464e0e1a9f3f543e6253
johnbian <byh19920310@126.com>
2022-07-28 19:17:56 +0800

    feat: 去掉注释

--- packages/agera-pad/packages/index.ts
+++ packages/agera-pad/packages/index.ts
@@ -20,7 +20,3 @@ if (typeof window !== 'undefined' && (window as any).Vue) {
 
 // To allow use as module (npm/webpack/etc.) export component
 export default { install, AgeraPadBtn };
-
-// It's possible to expose named exports when writing components that can
-// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
-// export const RollupDemoDirective = component;
